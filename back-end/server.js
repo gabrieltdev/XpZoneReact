60d9c29d4cdd0e15fae8d8fb3ce3102277bd24c8
Gabriel Tavares Tinoco <gabrieltavares.crvg@gmail.com>
2026-07-01 17:12:50 -0300

    Fix: Corrigindo importacao do UsersRoutes com U maiusculo

--- back-end/server.js
+++ back-end/server.js
@@ -15,7 +15,7 @@ server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
 //routes imports
-const users_routes = require("./routes/usersRoutes");
+const users_routes = require("./routes/UsersRoutes");
 const games_routes = require("./routes/GamesRoutes");
 const consoles_routes = require("./routes/ConsolesRoutes");
 const addresses_routes = require("./routes/AddressesRoutes");
